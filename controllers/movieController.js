4498a23928c1ae4680513b37e3191eda91d09a19
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-07 00:12:08 -0500

    partials made

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,8 +1,18 @@
 const movieDatabase = require("../db/db");
+const imagesForContent = [
+  "https://images2.vudu.com/poster2/1576343-168",
+  "https://images2.vudu.com/assets/content/poster/1493460-144",
+  "https://images2.vudu.com/assets/content/poster/1598398-144",
+  "https://images2.vudu.com/assets/content/poster/1503232-144",
+  "https://images2.vudu.com/assets/content/poster/730488-144",
+];
 
 module.exports.getMovies = (req, res) => {
   console.log(movieDatabase);
-  res.render("index", { movies: movieDatabase.movies });
+  res.render("index", {
+    movies: movieDatabase.movies,
+    images: imagesForContent,
+  });
 };
 
 module.exports.getMovie = (req, res) => {
