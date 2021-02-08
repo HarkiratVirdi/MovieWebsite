45453c6af3ada24aecd3044ed00a1a0e2c245bf6
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-08 18:47:38 -0500

    everything fine

--- public/js/barba.js
+++ public/js/barba.js
@@ -12,8 +12,44 @@ barba.init({
   ],
 });
 
+// let tl = gsap.timeline();
+// tl.staggerTo(".movie_char", 0.25, { opacity: 1 }, 0.03);
+
+// const movieDetailsWidth = document.querySelector(".movie_details").clientHeight;
+// console.log(movieDetailsWidth);
+// gsap.to(".movie_hero_image", {
+//   scrollTrigger: {
+//     trigger: ".movie_hero_image",
+//     scrub: true,
+//     start: "top center",
+//     toggleActions: "restart pause reverse pause",
+//   },
+//   transform: "matrix(1.1,0,0,1.1,0,0)",
+// });
+
+// // gsap.to(".purchasemovie", {
+// //   scrollTrigger: {
+// //     trigger: ".purchasemovie",
+// //     pin: true,
+// //     start: "top top",
+// //     toggleActions: "restart pause reverse pause",
+// //     markers: true,
+// //     end:
+// //   },
+// // });
+
+// let controller = new ScrollMagic.Controller();
+
+// const scene = new ScrollMagic.Scene({
+//   triggerElement: ".purchasemovie",
+//   triggerHook: "onLeave",
+//   duration: movieDetailsWidth,
+//   offset: -150,
+// })
+//   .setPin(".purchasemovie")
+//   .addTo(controller);
+
 function loaderIn() {
-  // GSAP tween to stretch the loading screen across the whole screen
   return gsap.fromTo(
     loader,
     {
@@ -59,4 +95,41 @@ gsap.set(loader, {
 });
 barba.hooks.enter(() => {
   window.scrollTo(0, 0);
+  let tl = gsap.timeline({});
+  tl.staggerTo(".movie_char", 0.25, { opacity: 1, delay: 0.6 }, 0.03);
+
+  const movieDetailsWidth = document.querySelector(".movie_details")
+    .clientHeight;
+  console.log(movieDetailsWidth);
+  gsap.to(".movie_hero_image", {
+    scrollTrigger: {
+      trigger: ".movie_hero_image",
+      scrub: true,
+      start: "top center",
+      toggleActions: "restart pause reverse pause",
+    },
+    transform: "matrix(1.1,0,0,1.1,0,0)",
+  });
+
+  // gsap.to(".purchasemovie", {
+  //   scrollTrigger: {
+  //     trigger: ".purchasemovie",
+  //     pin: true,
+  //     start: "top top",
+  //     toggleActions: "restart pause reverse pause",
+  //     markers: true,
+  //     end:
+  //   },
+  // });
+
+  let controller = new ScrollMagic.Controller();
+
+  const scene = new ScrollMagic.Scene({
+    triggerElement: ".purchasemovie",
+    triggerHook: "onLeave",
+    duration: movieDetailsWidth,
+    offset: -150,
+  })
+    .setPin(".purchasemovie")
+    .addTo(controller);
 });
--- public/js/effects.js
+++ public/js/effects.js
@@ -1,5 +1,8 @@
 let tl = gsap.timeline();
 tl.staggerTo(".movie_char", 0.25, { opacity: 1 }, 0.03);
+
+const movieDetailsWidth = document.querySelector(".movie_details").clientHeight;
+console.log(movieDetailsWidth);
 gsap.to(".movie_hero_image", {
   scrollTrigger: {
     trigger: ".movie_hero_image",
@@ -9,3 +12,25 @@ gsap.to(".movie_hero_image", {
   },
   transform: "matrix(1.1,0,0,1.1,0,0)",
 });
+
+// gsap.to(".purchasemovie", {
+//   scrollTrigger: {
+//     trigger: ".purchasemovie",
+//     pin: true,
+//     start: "top top",
+//     toggleActions: "restart pause reverse pause",
+//     markers: true,
+//     end:
+//   },
+// });
+
+let controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".purchasemovie",
+  triggerHook: "onLeave",
+  duration: movieDetailsWidth,
+  offset: -150,
+})
+  .setPin(".purchasemovie")
+  .addTo(controller);
