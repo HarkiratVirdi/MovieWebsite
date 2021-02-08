65c34b67c5c9839803818ffcfa1a80e508395710
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-08 11:48:26 -0500

    double carousel

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -6,11 +6,19 @@ const imagesForContent = [
   "https://images2.vudu.com/assets/content/poster/1503232-144",
   "https://images2.vudu.com/assets/content/poster/730488-144",
 ];
+const imagesForCarousel = [
+  "https://m.media-amazon.com/images/M/MV5BNGIzNWI4MjQtZWI4Zi00N2U4LTg1MWMtNmRmYmIxOWVjZTkwXkEyXkFqcGdeQWFybm8@._V1_QL40_UX1000_CR0,0,1000,563_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BNWMxOGUxN2MtMTBiMi00MWM3LTgzZTItZTYwZTZmYTQ0ZjQ0XkEyXkFqcGdeQXVyNzU3Nzk4MDQ@._V1_QL40_UX1000_CR0,0,1000,563_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BZmE1ZTgwZTEtMzU3YS00MWYzLTk3NzctZDMwZjQxNmRhMGY0XkEyXkFqcGdeQWFybm8@._V1_QL40_UX1000_CR0,0,1000,563_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMTFhOGJjZjUtOWM4Ny00ZWE0LWFlMDUtMDAyMTc2MTcxMmM3XkEyXkFqcGdeQWFybm8@._V1_QL40_UX1000_CR0,0,1000,563_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMDI0NmYxZmMtN2EyNi00YzMwLTgzNWEtYjY4NmIxMjlmMzJhXkEyXkFqcGdeQXVyNzE3ODQxNjU@._CR212,55,777,437.jpg",
+];
 
 module.exports.getMovies = (req, res) => {
   res.render("index", {
     movies: movieDatabase.movies,
     images: imagesForContent,
+    imagesForCarousel: imagesForCarousel,
   });
 };
 
