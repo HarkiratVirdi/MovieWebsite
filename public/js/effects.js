580be13277b742d218ffab1841b5cc77999d35a6
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-08 00:08:46 -0500

    buttons mad

--- public/js/effects.js
+++ public/js/effects.js
@@ -1,4 +1,11 @@
-window.onload = () => {
-  let tl = gsap.timeline();
-  tl.staggerTo(".movie_char", 0.25, { opacity: 1 }, 0.03);
-};
+let tl = gsap.timeline();
+tl.staggerTo(".movie_char", 0.25, { opacity: 1 }, 0.03);
+// gsap.to(".movie_hero_image", {
+//   scrollTrigger: {
+//     trigger: ".movie_hero_image",
+//     scrub: true,
+//     start: "top center",
+//     toggleActions: "restart pause reverse pause",
+//   },
+//   transform: "matrix(1.1,0,0,1.1,0,0)",
+// });
