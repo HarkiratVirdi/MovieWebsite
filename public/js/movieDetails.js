5871209d3a5b7c66a10eeee801a218154057a737
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-09 19:53:34 -0500

    refractor

--- public/js/movieDetails.js
+++ public/js/movieDetails.js
@@ -2,6 +2,7 @@ let tl = gsap.timeline();
 tl.staggerTo(".movie_char", 0.25, { opacity: 1 }, 0.03);
 
 const movieDetailsHeight = document.querySelector(".movie_info").clientHeight;
+
 gsap.to(".movie_hero_image", {
   scrollTrigger: {
     trigger: ".movie_hero_image",
