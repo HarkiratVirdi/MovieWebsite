350ef488ee68a5fcdb7f22aa267b2eb57af4b2d8
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-15 17:00:42 -0500

    reviews api added

--- public/js/movieListCarousel.js
+++ public/js/movieListCarousel.js
@@ -1,7 +1,3 @@
-function isOverflown(element) {
-  return element.scrollWidth > element.clientWidth;
-}
-
 let currentScroll = 0;
 
 const movieList = document.querySelectorAll(".movie_list");
@@ -9,23 +5,23 @@ const movieListFeatured = document.querySelector(".movie_list_featured");
 const buttonLeft = document.querySelector(".movie_list_buttons .left");
 const buttonRight = document.querySelector(".movie_list_buttons .right");
 
+const isOverflown = (element) => {
+  return element.scrollWidth > element.clientWidth;
+};
+
 if (!isOverflown(movieList[0])) {
   movieList[0].parentElement.children[0].style.display = "none";
   movieList[0].parentElement.children[1].style.display = "none";
-  //   buttonLeft.style.display = "none";
-  //   buttonRight.style.display = "none";
 }
 
 if (!isOverflown(movieList[1])) {
   movieList[1].parentElement.children[0].style.display = "none";
   movieList[1].parentElement.children[1].style.display = "none";
-  //   buttonRight.style.display = "none";
 }
 
 const scroll = (e) => {
-  console.log("scroll works", e.target);
   const target = e.target.nextElementSibling;
-  console.log("curent scroll", currentScroll);
+
   if (currentScroll <= movieListFeatured.clientWidth) {
     if (e.target.classList.contains("right")) {
       currentScroll += window.innerWidth / 2;
@@ -33,7 +29,6 @@ const scroll = (e) => {
       if (currentScroll >= movieListFeatured.clientWidth) {
         currentScroll = movieListFeatured.clientWidth - window.innerWidth / 2;
       }
-      console.log("contains right", currentScroll);
       target.scrollTo(currentScroll, 0);
     } else {
       currentScroll -= window.innerWidth / 2;
@@ -42,7 +37,6 @@ const scroll = (e) => {
         currentScroll = 0;
       }
 
-      console.log("contains left", currentScroll);
       target.nextElementSibling.scrollTo(currentScroll, 0);
     }
   }
