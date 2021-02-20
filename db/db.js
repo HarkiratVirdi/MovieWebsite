4ff06614fe89dbfb3f2a8cdfff3a0f98fe32e79c
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-20 12:42:52 -0500

    fixed images

--- db/db.js
+++ db/db.js
@@ -6,8 +6,8 @@ module.exports.movies = [
     rating: "60",
     rent: "3",
     buy: "15",
-    img_s: "https://images2.vudu.com/poster2/7652-168",
-    img_l: "https://images2.vudu.com/background/7652-1280a.jpg",
+    img_s: "/images/Die_Hard_s.jpg",
+    img_l: "/images/Die_Hard_l.jpg",
     isMovie: true,
     genre: "Action, Adventure",
     studio: "Warner Bros",
@@ -40,8 +40,8 @@ module.exports.movies = [
     rent: "2",
     rated: "PG-17",
     buy: "12",
-    img_s: "https://images2.vudu.com/poster2/456779-168",
-    img_l: "https://images2.vudu.com/background/456779-1280a.jpg",
+    img_s: "/images/Conjuring_s.jpg",
+    img_l: "/images/Conjuring_l.jpg",
     isMovie: true,
     synopsis:
       "Buy any quality, get every quality: All qualities up to 4K UHD included with purchase. Before there was Amityville, there was Harrisville. The Conjuring tells the horrifying true story of Ed and Lorraine Warren, world renowned paranormal investigators, who were called to help a family terrorized by a dark presence in a secluded farmhouse. Forced to confront a powerful demonic entity, the Warrens find themselves caught in the most terrifying case of their lives.",
@@ -68,8 +68,8 @@ module.exports.movies = [
     runtime: "1:57",
     year: "2017",
     rent: "10",
-    img_s: "https://images2.vudu.com/poster2/845903-168",
-    img_l: "https://images2.vudu.com/background/845903-1280a.jpg",
+    img_s: "/images/Wonder_Woman_s.jpg",
+    img_l: "/images/Wonder_Woman_l.jpg",
     isMovie: true,
     synopsis:
       "Buy any quality, get every quality: All qualities up to 4K UHD included with purchase. Before she was Wonder Woman, she was Diana, princess of the Amazons. Fighting alongside man in a war to end all wars, Diana will discover her full powers...and her true destiny. Watch it in Extras+ mode, available on the Vudu mobile app, and experience bonuses synced to the movie, movie trivia, a digital comic, and so much more. See it before you try it in the Free Clips tab.",
@@ -101,8 +101,8 @@ module.exports.movies = [
     isMovie: true,
     synopsis:
       "Armed with only one word--Tenet-- and fighting for the survival of the entire world, the Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
-    img_s: "https://images2.vudu.com/assets/content/poster/1493460-144",
-    img_l: "https://images2.vudu.com/background/1493460-1280a.jpg",
+    img_s: "/images/Tenet_s.jpg",
+    img_l: "/images/Tenet_l.jpg",
     cast: [
       {
         name: "John David",
@@ -130,8 +130,8 @@ module.exports.movies = [
     isMovie: false,
     synopsis:
       "The long winter is here. Throughout seven thrilling episodes, the penultimate Season 8 of this blockbuster hit series focuses on a convergence of armies and attitudes that have been brewing for years.",
-    img_s: "https://images2.vudu.com/poster2/1101209-168",
-    img_l: "https://images2.vudu.com/background/1101209-1280a.jpg",
+    img_s: "/images/GOT_8_s.jpg",
+    img_l: "/images/GOT_8_l.jpg",
     cast: [
       {
         name: "Emilia Clarke",
@@ -159,8 +159,8 @@ module.exports.movies = [
     year: "2010",
     synopsis:
       "AMC's THE WALKING DEAD returns for a terrifying and exhilarating new season. In this post-apocalyptic world, Rick Grimes and his band of survivors continue their ongoing struggle to survive the threat of walkers as well as the dangers that lurk among the living. In the new season, we find Rick and the group fostering a thriving community in the safe haven of the prison. In this brutal world, the group's home and new way of life will be thoroughly tested, and their struggle to survive has never been so perilous.",
-    img_s: "https://images2.vudu.com/poster2/482719-168",
-    img_l: "https://images2.vudu.com/background/482719-1280a.jpg",
+    img_s: "/images/Walking_Dead_s.jpg",
+    img_l: "/images/Walking_Dead_l.jpg",
     cast: [
       {
         name: "Andrew Lincoln",
@@ -189,8 +189,8 @@ module.exports.movies = [
     isMovie: true,
     synopsis:
       "The supernatural thriller brings to the screen another real case from the files of renowned demonologists Ed and Lorraine Warren. Reprising their roles, Vera Farmiga and Patrick Wilson star as Lorraine and Ed",
-    img_s: "https://images2.vudu.com/poster2/776990-168",
-    img_l: "https://images2.vudu.com/background/776990-1280a.jpg",
+    img_s: "/images/Conjuring2_s.jpg",
+    img_l: "/images/Conjuring2_l.jpg",
     cast: [
       {
         name: "Vera Farmiga",
@@ -218,8 +218,8 @@ module.exports.movies = [
     isMovie: true,
     synopsis:
       "When a scientific expedition to an uncharted island awakens titanic forces of nature, a mission of discovery becomes an explosive war between monster and man. Tom Hiddleston, Samuel L. Jackson, Brie Larson, John Goodman and John C. Reilly star in a thrilling and original adventure that reveals the untold story of how Kong became King. FOR 3D ENABLED TVs ONLY.",
-    img_s: "https://images2.vudu.com/poster2/876801-168",
-    img_l: "https://images2.vudu.com/background/876801-1280a.jpg",
+    img_s: "/images/Kong_Skull_s.jpg",
+    img_l: "/images/Kong_Skull_l.jpg",
     cast: [
       {
         name: "Brie Larsson",
@@ -247,8 +247,8 @@ module.exports.movies = [
     year: "2013",
     synopsis:
       "From director Steve McQueen, and based on Solomon Northup's astonishing true story, comes this powerful picture. In 1841, Northup (Chiwetel Ejiofor), a free citizen, is kidnapped, stripped of his identity and sold into slavery. Now, he must find the strength to survive in this unflinching story of hope.",
-    img_s: "https://images2.vudu.com/poster2/481186-168",
-    img_l: "https://images2.vudu.com/background/481186-1280a.jpg",
+    img_s: "/images/12Years_s.jpg",
+    img_l: "/images/12Years_l.jpg",
     cast: [
       {
         name: "Chiwetel Ejiofor",
@@ -276,9 +276,8 @@ module.exports.movies = [
     isMovie: false,
     synopsis:
       "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
-    img_s: "https://images2.vudu.com/poster2/207622-168",
-    img_l:
-      "https://images.amcnetworks.com/amc.com/wp-content/uploads/2012/06/BB-S5-Key-Art-2560-v2.jpg",
+    img_s: "/images/Breaking_Bad_s.jpg",
+    img_l: "/images/Breaking_Bad_l.jpg",
     cast: [
       {
         name: "Bryan Cranston",
@@ -306,8 +305,8 @@ module.exports.movies = [
     isMovie: false,
     synopsis:
       "A plethora of compelling storylines play out to their inevitable, often bloody conclusions in Season 5 of GAME OF THRONES. In the wake of the numerous shocking deaths of Season 4, the season opens with a power vacuum that the various players all across Westeros and Essos scramble to fill.",
-    img_s: "https://images2.vudu.com/poster2/673563-168",
-    img_l: "https://images2.vudu.com/background/673563-1280a.jpg",
+    img_s: "/images/GOT_4_s.jpg",
+    img_l: "/images/GOT_4_l.jpg",
     cast: [
       {
         name: "Emilia Clarke",
@@ -335,8 +334,8 @@ module.exports.movies = [
     isMovie: true,
     synopsis:
       "Arrival is a 2016 American science fiction film directed by Denis Villeneuve and written by Eric Heisserer.It stars Amy Adams, Jeremy Renner, and Forest Whitaker. The film follows a linguist enlisted by the United States Army to discover how to communicate with extraterrestrial aliens who have arrived on Earth, before tensions lead to war.",
-    img_s: "https://images2.vudu.com/poster2/826376-168",
-    img_l: "https://images2.vudu.com/background/826376-1280a.jpg",
+    img_s: "/images/Arrival_s.jpg",
+    img_l: "/images/Arrival_l.jpg",
     cast: [
       {
         name: "Amy Adams",
