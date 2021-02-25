f6cffd742348d4d1bd8ecf3edce261b9b32101e8
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-25 14:18:15 -0500

    order model made

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,22 @@
 const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: [true, "Email is required"],
+    lowercase: true,
+    unique: true,
+    validate: [validator.isEmail, "An email is required"],
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: true,
+  },
+});
