162b79406d0af7bec81a665447fce861c0298bb8
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-03-04 15:54:15 -0500

    mouse drag functionality

--- public/js/cursor.js
+++ public/js/cursor.js
@@ -1,5 +1,5 @@
 const cursor = document.querySelector(".cursor_js");
-
+const swiperContainer = document.querySelectorAll(".swiper-container");
 const imgs = document.querySelectorAll("img");
 
 imgs.forEach((img) => {
@@ -24,6 +24,20 @@ const bodyScroll = () => {
   return document.documentElement.scrollTop;
 };
 
+const dragCursor = () => {
+  console.log("in Swiper container");
+  cursor.classList.add('cursor_js-drag');
+}
+
+const normalCursor = () => {
+  cursor.classList.remove('cursor_js-drag');
+}
+
 document.addEventListener("mousemove", animateCursor);
+
+swiperContainer.forEach(element => {
+  element.addEventListener("mouseenter", dragCursor)
+  element.addEventListener('mouseleave', normalCursor);
+});
 
 window.addEventListener("scroll", bodyScroll);
