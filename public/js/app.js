c3274826f9b1cb3ec124703a9d356309f71ac546
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-03-04 14:44:20 -0500

    vh set

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,14 @@ import "./cursor.js";
 import "./menu.js";
 // import "./barba.js";
 // AOS.init();
+
+
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
