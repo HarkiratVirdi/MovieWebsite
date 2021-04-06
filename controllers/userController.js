0c3fa0ce1267a6fc012c33715bd72ec9d8139b0f
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-04-06 17:21:24 -0400

    making orders how

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,14 +5,16 @@ const {
   validatePassword,
   validateName,
 } = require("../utils/Validation");
-const {randomImage, sendMail} = require("../utils/utils");
+const {
+  randomImage,
+  sendMail
+} = require("../utils/utils");
 const movieModel = require("../models/movieModel");
 const orderModel = require("../models/orderModel");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 let YOUR_DOMAIN = "http://localhost:5000";
-if(process.env.NODE_ENV === "production")
-{
+if (process.env.NODE_ENV === "production") {
   YOUR_DOMAIN = "https://mflixharkirat.herokuapp.com";
 }
 
@@ -21,75 +23,86 @@ module.exports.loginUser = (req, res) => {
   const image = randomImage();
 
 
-  res.render("login", { image: image, title: "MovieNation | Login", values: req.body });
+  res.render("login", {
+    image: image,
+    title: "MovieNation | Login",
+    values: req.body
+  });
 };
 
 
-module.exports.signIn =  async(req, res) => {
-  const { Email, Password } = req.body;
- const isEmail = validateEmail(Email);
- const isPassword = validatePassword(Password);
- let errors = {
+module.exports.signIn = async (req, res) => {
+  const {
+    Email,
+    Password
+  } = req.body;
+  const isEmail = validateEmail(Email);
+  const isPassword = validatePassword(Password);
+  let errors = {
     Email: "",
     Password: "",
   };
 
   try {
-       const findUser = await userModel.findOne({email: Email})
-       
-       if(findUser)
-       {
-         const match = await bcrypt.compare(Password, findUser.password);
-         if (!match) {
-          throw 1;
-        }
-        
-        req.session.userInfo = findUser;
+    const findUser = await userModel.findOne({
+      email: Email
+    })
 
-        
-        if(req.session.userInfo.isAdmin )
-        { res.redirect("/user/admin");
-        }else{
-       res.redirect("/user/dashboard");
-        }
-    
-        
-      }else{
-        throw 2;
+    if (findUser) {
+      const match = await bcrypt.compare(Password, findUser.password);
+      if (!match) {
+        throw 1;
       }
-}catch (error) {
-  if(error === 1)
-  {
-    errors.Password = "Your Email or/and Password is Incorrect";
-  }
 
-  if(error === 2)
-  {
-    errors.Email = "Email does not exist. Please Register first";
-  }
+      req.session.userInfo = findUser;
 
-   if (!isEmail) {
-     errors.Email = "Email must be valid";
-   
-   }
+
+      if (req.session.userInfo.isAdmin) {
+        res.redirect("/user/admin");
+      } else {
+        res.redirect("/user/dashboard");
+      }
+
+
+    } else {
+      throw 2;
+    }
+  } catch (error) {
+    if (error === 1) {
+      errors.Password = "Your Email or/and Password is Incorrect";
+    }
+
+    if (error === 2) {
+      errors.Email = "Email does not exist. Please Register first";
+    }
+
+    if (!isEmail) {
+      errors.Email = "Email must be valid";
+
+    }
 
     if (!isPassword) {
-       errors.Password =
-         "Password must have length of 8 characters including 1 letter, 1 number, 1 special character";
-  }
+      errors.Password =
+        "Password must have length of 8 characters including 1 letter, 1 number, 1 special character";
+    }
 
     const image = randomImage();
     res.render("login", {
-    image: image,
-    title: "MovieNation | Login",
-    errors: errors,
-    values: req.body,
-  });
-}
+      image: image,
+      title: "MovieNation | Login",
+      errors: errors,
+      values: req.body,
+    });
+  }
 };
 
 module.exports.signUp = async (req, res) => {
-  const { Email, Password, Firstname, Lastname } = req.body;
+  const {
+    Email,
+    Password,
+    Firstname,
+    Lastname
+  } = req.body;
   const isEmail = validateEmail(Email);
   const isPassword = validatePassword(Password);
   const isFirstName = validateName(Firstname);
@@ -102,80 +115,80 @@ module.exports.signUp = async (req, res) => {
   };
 
   let valid = true;
-     if (!isEmail || Email === "") {
-       errors.Email = "Email must be valid";
-      valid = false;
-      }
-     if (!isLastName || Lastname === "") {
-       errors.Lastname = "Last Name should only contain Alphabets";
-      valid = false;
-     
-      }
-     if (!isFirstName || Firstname === "") {
-       errors.Firstname = "First Name should only contain Alphabets";
-      valid = false;
-     
-      }
-     if (!isPassword || Password === "") {
-       errors.Password =
-         "Password must have length of 8 characters including 1 letter, 1 number, 1 special character";
-      valid = false;
-        } 
+  if (!isEmail || Email === "") {
+    errors.Email = "Email must be valid";
+    valid = false;
+  }
+  if (!isLastName || Lastname === "") {
+    errors.Lastname = "Last Name should only contain Alphabets";
+    valid = false;
 
-    try {
-      const newUser = new userModel({
-        firstname: Firstname,
-        lastname: Lastname,
-        email: Email,
-        password: Password,
-      });
+  }
+  if (!isFirstName || Firstname === "") {
+    errors.Firstname = "First Name should only contain Alphabets";
+    valid = false;
 
-      if (!valid || !newUser) { 
-        throw "Incorrect Credentials Entered";
-      }else{
-          const user = await newUser.save();
-          const mailSent = await sendMail(user);
-          if (user && mailSent) {
-            user.cart = [];
-            req.session.userInfo = user;
-            console.log("session after login", user);
-            console.log("session2 after login", req.session.userInfo);
-            res.redirect("/user/dashboard");
-          } else {
-            throw "Unable to Register User";
-          }
-      }
-    } catch (err) {
-      if(err.code === 11000)
-      { 
-        errors.Email = "Email is Already Registered. Please Login Instead";
-      }
+  }
+  if (!isPassword || Password === "") {
+    errors.Password =
+      "Password must have length of 8 characters including 1 letter, 1 number, 1 special character";
+    valid = false;
+  }
 
-        const image = randomImage();
-        res.render("register", {
-          image: image,
-          title: "MovieNation | Register",
-          errors: errors,
-          values: req.body,
-        });
-    }
-};
-
-module.exports.adminDashboard = async(req, res) => {
   try {
-    const allMovies = await movieModel.find({}).lean();
-    if(allMovies)
-    {
-      res.render("Admin", {movies: allMovies});
+    const newUser = new userModel({
+      firstname: Firstname,
+      lastname: Lastname,
+      email: Email,
+      password: Password,
+    });
+
+    if (!valid || !newUser) {
+      throw "Incorrect Credentials Entered";
+    } else {
+      const user = await newUser.save();
+      const mailSent = await sendMail(user);
+      if (user && mailSent) {
+        user.cart = [];
+        req.session.userInfo = user;
+        console.log("session after login", user);
+        console.log("session2 after login", req.session.userInfo);
+        res.redirect("/user/dashboard");
+      } else {
+        throw "Unable to Register User";
+      }
     }
   } catch (err) {
-      console.log("error", err);
+    if (err.code === 11000) {
+      errors.Email = "Email is Already Registered. Please Login Instead";
+    }
+
+    const image = randomImage();
+    res.render("register", {
+      image: image,
+      title: "MovieNation | Register",
+      errors: errors,
+      values: req.body,
+    });
+  }
+};
+
+module.exports.adminDashboard = async (req, res) => {
+  try {
+    const allMovies = await movieModel.find({}).lean();
+    if (allMovies) {
+      res.render("Admin", {
+        movies: allMovies
+      });
+    }
+  } catch (err) {
+    console.log("error", err);
   }
 }
 
-module.exports.logout = (req,res) => {
-    req.session.destroy();
-    res.redirect("/user/login");
+module.exports.logout = (req, res) => {
+  req.session.destroy();
+  res.redirect("/user/login");
 }
 
 module.exports.deleteUserPage = (req, res) => {
@@ -185,11 +198,18 @@ module.exports.deleteUserPage = (req, res) => {
 module.exports.updateUserPage = (req, res) => {
   const image = randomImage();
 
-  res.render("updateUser", {image: image});
+  res.render("updateUser", {
+    image: image
+  });
 }
 
 module.exports.updateUser = async (req, res) => {
-   const { Email, Firstname, Lastname, Password } = req.body;
+  const {
+    Email,
+    Firstname,
+    Lastname,
+    Password
+  } = req.body;
   const isEmail = validateEmail(Email);
   const isPassword = validatePassword(Password);
   const isFirstName = validateName(Firstname);
@@ -202,78 +222,75 @@ module.exports.updateUser = async (req, res) => {
   };
 
   let valid = true;
-     if (!isEmail || Email === "") {
-       errors.Email = "Email must be valid";
-      valid = false;
-      }
-     if (!isLastName || Lastname === "") {
-       errors.Lastname = "Last Name should only contain Alphabets";
-      valid = false;
-     
-      }
-     if (!isFirstName || Firstname === "") {
-       errors.Firstname = "First Name should only contain Alphabets";
-      valid = false;
-      }
-        if (!isPassword || Password === "") {
-          errors.Password =
-            "Password must have length of 8 characters including 1 letter, 1 number, 1 special character";
-          valid = false;
-        } 
+  if (!isEmail || Email === "") {
+    errors.Email = "Email must be valid";
+    valid = false;
+  }
+  if (!isLastName || Lastname === "") {
+    errors.Lastname = "Last Name should only contain Alphabets";
+    valid = false;
 
-    try {
-      if (!valid) { 
-        throw "Incorrect Credentials Entered";
-      }else{
+  }
+  if (!isFirstName || Firstname === "") {
+    errors.Firstname = "First Name should only contain Alphabets";
+    valid = false;
+  }
+  if (!isPassword || Password === "") {
+    errors.Password =
+      "Password must have length of 8 characters including 1 letter, 1 number, 1 special character";
+    valid = false;
+  }
 
-          const user = await userModel.findById(res.locals.user._id);
+  try {
+    if (!valid) {
+      throw "Incorrect Credentials Entered";
+    } else {
 
-          if(user)
-          { 
-            user.firstname = Firstname;
-            user.lastname = Lastname;
-            user.password = Password;
-            user.email = Email;
-          }
+      const user = await userModel.findById(res.locals.user._id);
 
-          const updateUser = await user.save();
-
-          if(updateUser)
-          {
-            console.log("user updated", updateUser);
-            req.session.userInfo = updateUser;
-            res.redirect("/user/dashboard");
-          }
-
-
-      }
-    } catch (err) {
-      if(err.code === 11000)
-      { 
-        errors.Email = "Email is Already Registered. Please use another Email";
+      if (user) {
+        user.firstname = Firstname;
+        user.lastname = Lastname;
+        user.password = Password;
+        user.email = Email;
       }
 
-        const image = randomImage();
-        res.render("updateUser", {
-          image: image,
-          title: "MovieNation | UpdateUser",
-          errors: errors,
-          values: req.body,
-        });
+      const updateUser = await user.save();
+
+      if (updateUser) {
+        console.log("user updated", updateUser);
+        req.session.userInfo = updateUser;
+        res.redirect("/user/dashboard");
+      }
+
+
     }
+  } catch (err) {
+    if (err.code === 11000) {
+      errors.Email = "Email is Already Registered. Please use another Email";
+    }
+
+    const image = randomImage();
+    res.render("updateUser", {
+      image: image,
+      title: "MovieNation | UpdateUser",
+      errors: errors,
+      values: req.body,
+    });
+  }
 }
 
 module.exports.deleteUser = async (req, res) => {
   try {
     const deleteUser = await userModel.findByIdAndDelete(res.locals.user);
-     if (deleteUser) {
-        req.session.destroy();
-         res.redirect("/user/register?account=true");
-     } else {
+    if (deleteUser) {
+      req.session.destroy();
+      res.redirect("/user/register?account=true");
+    } else {
       throw "unable to delete user";
-     }
+    }
   } catch (error) {
-   console.log("not deleted", deleteUser);
+    console.log("not deleted", deleteUser);
     res.redirect("/user/delete");
   }
 }
@@ -285,92 +302,100 @@ module.exports.dashboard = (req, res) => {
 module.exports.registerUser = (req, res) => {
   const image = randomImage();
 
-    let account = false;
-    if (req.query.account) {
-      account = true;
-    }
+  let account = false;
+  if (req.query.account) {
+    account = true;
+  }
 
-  res.render("register", { image: image, title: "MovieNation | Register", account: account });
+  res.render("register", {
+    image: image,
+    title: "MovieNation | Register",
+    account: account
+  });
 };
 
 
 module.exports.checkout = async (req, res) => {
-   
-  const {moviesInCart, tax, subtotal, total} = res.locals.cartDetails;
+
+  const {
+    moviesInCart,
+    tax,
+    subtotal,
+    total
+  } = res.locals.cartDetails;
 
   res.render("checkout", {
-        moviesInCart,
-        subtotal,
-        tax: parseFloat(tax).toFixed(2),
-        total,
-        title: "Mflix | Checkout",
-      });
-  }
+    moviesInCart,
+    subtotal,
+    tax: parseFloat(tax).toFixed(2),
+    total,
+    title: "Mflix | Checkout",
+  });
+}
 
-module.exports.getOrders = async(req, res) => {
+module.exports.getOrders = async (req, res) => {
   try {
-    const orders = await orderModel.find({user: res.locals.user._id}).lean();
+    const orders = await orderModel.find({
+      user: res.locals.user._id
+    }).lean();
 
     if(orders)
     {
-       console.log("orders **", orders);
+      const allOrders = [];
+      orders.forEach(async(el, indexOuter, arrOrder) => {
+        // console.log("orderItems", el.orderItems, indexOuter);
+        let singleOrder = [];
+        el.orderItems.forEach(async(orderItem, i, arrOrderItem) => {
+            const movieInOrder = await movieModel
+              .findOne({
+                _id:
+                  orderItem.movieId,
+              })
+              .lean();
 
-      const orderItemsMovies = []; 
+                if (movieInOrder) {
+                  orderItem = {...orderItem, ...movieInOrder};
 
-        orders.forEach(async(order) => { 
-          console.log("each order outest", order);
-        let orderMovies = await order.orderItems.map(async(el) => {
-          let movieDetails = await movieModel.find({_id: el.movieId});
-          
-          if(orderMovies)
-          {
-            if(movieDetails)
-            {
-           orderItemsMovies.push({
-            isBuying: el.isBuying,
-            price: el.price,
-            movieDetails,
-          });
+                  // console.log("order item", orderItem, indexOuter);
+                  singleOrder.push(orderItem);
+                  allOrders[indexOuter] = singleOrder;
+                  // console.log("all orders **********", allOrders, indexOuter);
 
-               console.log(
-                 "orderItemsMovies********************",
-                 orderItemsMovies
-               );
-                
-               if(orders.length === orderItemsMovies.length)
-               {
-                      res.render("orders", {
-                        orders,
-                        orderItemsMovies,
-                      });
-
-               }
-        
-        }
-      }  
-    })
-    })
-
- 
- 
+                  if(i === arrOrderItem.length - 1 && indexOuter === arrOrder.length - 1)
+                  {
+                    console.log("all Order", allOrders[0]);
+                    res.render("orders", {
+                      allOrders: allOrders[0]
+                    })
+                  }
+                }
+        })
+        })
+        // console.log("all orders ", allOrders);
     }
+
+
   } catch (err) {
-    console.log("error fetching orders",err);
+
   }
 }
 
 
-module.exports.payment = async(req, res) => {
- const { moviesInCart, tax, subtotal,total } = res.locals.cartDetails;
+module.exports.payment = async (req, res) => {
+  const {
+    moviesInCart,
+    tax,
+    subtotal,
+    total
+  } = res.locals.cartDetails;
 
   try {
     const product_data = moviesInCart.map((el) => {
       let price = el.buy;
-      if(!el.isBuying) 
-      {
-        price = el.rent; 
+      if (!el.isBuying) {
+        price = el.rent;
       }
-    
+
       return {
         movieId: el._id,
         isBuying: el.isBuying,
@@ -378,9 +403,9 @@ module.exports.payment = async(req, res) => {
       };
     });
 
-    
-console.log("product_data", product_data);
-    
+
+    console.log("product_data", product_data);
+
     const newOrder = {
       user: res.locals.user._id,
       orderItems: product_data,
@@ -391,14 +416,12 @@ console.log("product_data", product_data);
     console.log("new order", newOrder);
 
     const order = await new orderModel(newOrder).save();
-    if(order)
-    {
+    if (order) {
       console.log("order", order);
       res.redirect("/user/orders");
     }
-  } 
-catch (error) {
-      console.log("error inserting order to model", error);
+  } catch (error) {
+    console.log("error inserting order to model", error);
   }
 
 
